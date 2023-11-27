fcc41b010e639e3b10ac657133a74c04a2d363aa
Manh Phan <dphan8@uic.edu>
2023-11-27 00:04:25 -0600

    reduce redundant code in puzzleArduino

--- puzzleArduino/puzzleArduino.ino
+++ puzzleArduino/puzzleArduino.ino
@@ -1,9 +1,7 @@
 #include <LiquidCrystal.h>
 
 
-//puzzle1 globals
-const int led1 = 10;
-const int led2 = 13;
+// PUZZLE 1 GLOBALS
 const int button1 = 6;
 const int button2 = 7;
 const int button3 = 8;
@@ -23,30 +21,18 @@ int button1State, button2State,
 int buttonStatus1, buttonStatus2, 
     buttonStatus3, buttonStatus4;
 
-//interval for leds to cycle
-int ledCycleCount = 0;
-unsigned long ledTime = 0;
-const long ledInterval = 1000;
-
-
-
-
-
-//puzzle2 globals
-// Define the LCD dimensions and global variables
-const int LCD_COLS = 16;
-const int LCD_ROWS = 2;
+// PUZZLE 2 GLOBALS
 
 // Initialize the LCD
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 //questions to give to player to answer
 char* questionArray[5] = {
-"What is 1+1?    ", 
-"What year was UIC founded 19XX?    ",
-"Two years ago, I was three times as old as my brother was. In three years time I will be twice as old as my brother.” How old is the younger brother in three years?",
-"My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What is the lower of the two house numbers?",
-"What is the smallest whole number that is equal to seven times the sum of its digits?"};
+"                What is 1+1?    ", 
+"                What year was UIC founded 19XX?    ",
+"                Two years ago, I was three times as old as my brother was. In three years time I will be twice as old as my brother. How old is the younger brother in three years?",
+"                My twin lives at the reverse of my house number. The difference between our house numbers ends in two. What is the lower of the two house numbers?",
+"                What is the smallest whole number that is equal to seven times the sum of its digits?"};
 int answerTens[5] = {0, 8, 1, 1, 2};
 int answerOnes[5] = {2, 2, 0, 9, 1};
 int questionNumber = 0;
@@ -80,7 +66,7 @@ unsigned long shiftMillis = 0;
 const int shiftInterval = 250;
 unsigned long currentMillis = 0;
 
-// puzzle 3
+// PUZZLE 3 GLOBALS
 int brown = 13, white = A3, black = A2, green = 10; 
 int valBrown, valWhite, valBlack, valGreen;
 
@@ -88,11 +74,10 @@ int valBrown, valWhite, valBlack, valGreen;
 char signalReceived, signalSent;
 int solved1, solved2, solved3; // 0 means not solved, 1 means solved.
 int trigger1, trigger2, trigger3;
-int triggerHint3;
 int gameStatus; // 1 means games on going, 0 means game finished
 
 // DEBUG variable
-int DEBUG = 0; 
+int DEBUG = 1; 
 
 
 // lcd functions
@@ -112,12 +97,9 @@ void freshStart() {
     questionNumber = 0;
     updateGlobals();
     memset(answerArray, -1, sizeof(answerArray));
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Hi players!");
+    writeRow(0, "Hi players!");
     solved1 = 0; solved2 = 0; solved3 = 0;
     trigger1 = 0; trigger2 = 0; trigger3 = 0;
-    triggerHint3 = 0;
     gameStatus = 0;
 }
 
@@ -131,17 +113,11 @@ void freshStart() {
 void updateFromMain() {
     if (Serial.available()) {
         signalReceived = Serial.read();
-        if (DEBUG) {
-            char output[100];
-            sprintf(output, "Signal R: %c", signalReceived);
-            writeRow(0, output);
-            //Serial.print("Signal received: "); Serial.println(signalReceived);
-        }
 
         if (signalReceived == '1') {
             if (!trigger1 || !trigger3) {
                 
-                writeRow(0, "start game");
+                writeRow(1, "start P1 & P3");
                 gameStatus = 1;
                 trigger1 = 1;   // trigger puzzle 1
                 trigger3 = 1;   // trigger puzzle 3
@@ -150,26 +126,19 @@ void updateFromMain() {
         else if (signalReceived == '2') {
             if (!trigger2 && solved1) {
                 if (DEBUG) {
-                    writeRow(0, "trigger 2");
-                    //Serial.println("trigger puzzle 2");
+                    writeRow(1, "trigger 2");
                 }
                 trigger2 = 1; // trigger puzzle 2
             }
         }
         else if (signalReceived == '3') {
-            if (!trigger3 && solved2) {
-                if (DEBUG) {
-                    writeRow(0, "trigger hint 3");
-                    // Serial.println("trigger hint for puzzle 3");
-                }
-                triggerHint3 = 1; // trigger hint for puzzle 3 (puzzle 3 always run from the start)
+            if (solved2) {
+                writeRow(1, "Check Your Phone");
+                writeRow(0, "Hint: 25225"); // hint for puzzle 3
             }
         }
         else if (signalReceived == '4') {
-            if (DEBUG) {
-                writeRow(0, "trigger reset");
-                //Serial.println("trigger reset");
-            }
+            writeRow(1, "trigger reset");
             freshStart();
         }
     }
@@ -221,14 +190,6 @@ void questionInput() {
     writeRow(1, inputString);
 }
 
-
-//globals to help manage the text scrolling
-// char* message;
-// int length;
-// char copy[500];
-// char* temp;
-// char* end;
-// char holder;
 //used to make sure that the text is properly aligned when the question changes
 void updateGlobals() {
     //strcpy(message, questionArray[questionNumber]);
@@ -241,25 +202,19 @@ void updateGlobals() {
 
 //checks the tens and ones to ensure that it is of the correct number when 
 void validateAnswer() {
-    Serial.println("Submitting!");
     if (tens == answerTens[questionNumber] &&
         ones == answerOnes[questionNumber]) {
         questionNumber++;
-        if (questionNumber < 5) {
+        if (questionNumber < 3) {
             updateGlobals();
         }
-        Serial.println("correct");
-    } else {
-        //wrong answer lose time
-        Serial.println("wrong");
     }
-    if (questionNumber >= 5) {
+    if (questionNumber >= 3) {
         //puzzle is solved!
         solved2 = 1;
-        Serial.println("solved!");
         lcd.clear();
-        writeRow(0, "Hint: 25225");
-        writeRow(1, "Use your phone!");
+        writeRow(1, "P2 solved!");
+        Serial.write('2'); // send to main arduino
     }
 }
 
@@ -274,48 +229,6 @@ void brooklynDebouncePuzzle2(int buttonState, int *buttonStatus, int button) {
         }
     }
     *buttonStatus = buttonState;
-}
-
-
-
-void ledCycle () {
-
-    int cycleNum = ledCycleCount % 6;
-
-    //int correctAnswer[4] = {3,0,1,2};
-    if (cycleNum == 0) {
-
-        //display 3
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, HIGH);
-    } else if (cycleNum == 1) {
-
-        //display 0
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-    } else if (cycleNum == 2) {
-
-        //display 1
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, LOW);
-    } else if (cycleNum == 3) {
-
-        //display 2
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, HIGH);
-    } else if (cycleNum == 4) {
-        
-        // empty to reset and not confuse player
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-    } else if (cycleNum == 5) {
-        
-        //empty to reset and not confuse player
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-    }
-    //Serial.println(cycleNum);
-    ledCycleCount++;
 }
 
 void checkAnswer () {
@@ -354,7 +267,7 @@ void checkButtonInput (int button) {
             break;
     }
     char currentInput[20];
-    sprintf(currentInput, "[%d,%d,%d,%d]", answerArray[0], answerArray[1], answerArray[2], answerArray[3]);
+    sprintf(currentInput, "[%d, %d, %d, %d]", answerArray[0], answerArray[1], answerArray[2], answerArray[3]);
     writeRow(0, currentInput);
     count++;
     if (index == 3) {
@@ -384,11 +297,6 @@ void puzzle1() {
         brooklynDebouncePuzzle1(button4State, &buttonStatus4, button4);
         prevMillis = currentMillis;
     }
-    if (currentMillis - ledTime > ledInterval) {
-        ledCycle();
-        ledTime = currentMillis;
-        //Serial.println(ledTime);
-    }
 }
 
 void puzzle2() {
@@ -403,7 +311,6 @@ void puzzle2() {
     }
     if (currentMillis - shiftMillis > shiftInterval) {
         displayQuestion();
-        //Serial.println(temp);
         shiftMillis = currentMillis;
     }
 }
@@ -413,18 +320,10 @@ void puzzle3() {
     valWhite = digitalRead(white);
     valBlack = digitalRead(black);
     valGreen = digitalRead(green);
-    if (DEBUG) {
-        char output[100];
-        sprintf(output, "br%d wh%d bl%d gr%d", valBrown, valWhite, valBlack, valGreen);
-        //Serial.println(output);
-        writeRow(1, output);
-    }
-    if (valBrown == 1 && valWhite == 1 && valBlack == 1 && valGreen == 1) {
-        //if (DEBUG) lcd.print("bomb ticking");
-    }
-    else if (valBrown == 0 || valWhite == 0 || valGreen == 0) {
-        lcd.clear();
-        writeRow(0, "Game over!");
+
+    if (valBrown == 0 || valWhite == 0 || valGreen == 0) {
+        writeRow(0, "Bomb Exploded ><");
+        writeRow(1, "   Game over!   ");
 
 
         Serial.write('4');
@@ -432,9 +331,8 @@ void puzzle3() {
     }
     else if (valBlack == 0) {
 
-        lcd.clear();
-        writeRow(0, "Congratulations");
-        //Serial.println("correct!");
+        writeRow(0, "Bomb Defused ^^");
+        writeRow(1, "Congratulations!");
 
         Serial.write('3');
         solved3 = 1;
@@ -444,53 +342,29 @@ void puzzle3() {
 
 void setup() {
     Serial.begin(9600);
-    // // puzzle 1
-    // pinMode(led1, OUTPUT); pinMode(led2, OUTPUT);
-    // pinMode(b1, INPUT); pinMode(b2, INPUT); pinMode(b3, INPUT); pinMode(b4, INPUT);
-    // // puzzle 2
-    lcd.begin(16, 2);
-    // pinMode(pot1, INPUT); pinMode(pot2, INPUT);
-    // pinMode(confirmButton, INPUT);
-    // puzzle 3
-    pinMode(brown, INPUT); pinMode(white, INPUT); pinMode(black, INPUT); pinMode(green, INPUT);
-
-    // fresh start the puzzle Arduino's state variables
-    freshStart();
-    // Initialize the serial port
-    Serial.begin(9600);
-    pinMode(pot1, INPUT);
-    pinMode(pot2, INPUT);
-  
-
-    // Initialize the LCD
-    
-    lcd.setCursor(0,0);
-    pinMode(submitButton, INPUT);
-    submitStatus = digitalRead(submitButton);
-
-    updateGlobals();
-
-    //puzzle1 pins
-    pinMode(button1, INPUT);
-    pinMode(button2, INPUT);
-    pinMode(button3, INPUT);
-    pinMode(button4, INPUT);
-
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-
-    Serial.begin(9600);
+    // puzzle 1
+    pinMode(button1, INPUT); pinMode(button2, INPUT); pinMode(button3, INPUT); pinMode(button4, INPUT);
     buttonStatus1 = digitalRead(button1);
     buttonStatus2 = digitalRead(button2);
     buttonStatus3 = digitalRead(button3);
     buttonStatus4 = digitalRead(button4);
+
+    // puzzle 2
+    lcd.begin(16, 2); lcd.setCursor(0,0);
+    pinMode(submitButton, INPUT);
+    submitStatus = digitalRead(submitButton);
+    pinMode(pot1, INPUT); pinMode(pot2, INPUT);
+
+    // puzzle 3
+    pinMode(brown, INPUT); pinMode(white, INPUT); pinMode(black, INPUT); pinMode(green, INPUT);
+
+    // fresh start the puzzle Arduino's state variables
+    freshStart();    
 }
 
 void loop() {
-    //ledCycle();
     currentMillis = millis();
     updateFromMain();
-    //Serial.print("Game status "); Serial.println(gameStatus);
     if (gameStatus) {
         if (trigger1 && !solved1) puzzle1();
         if (trigger2 && !solved2) puzzle2();
